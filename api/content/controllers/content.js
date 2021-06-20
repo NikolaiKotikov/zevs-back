a34abe20de293e9544ddbcd2c37e2a02de3e2b96
nik <nikolai.kotikov@gmail.com>
2021-06-20 15:15:09 +0300

    feat: :sparkles: add catalog

--- api/content/controllers/content.js
+++ api/content/controllers/content.js
@@ -1,8 +1,45 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve the record.
+   *
+   * @return {Object}
+   */
+
+  async find(ctx) {
+    const entity = await strapi.services.content.find();
+    const content = sanitizeEntity(entity, { model: strapi.models.content });
+
+    for (const section of content.sections) {
+      if (section.__component === "section.section-catalog") {
+        const newItems = await Promise.all(
+          section.items.map(async (element) => {
+            const id = element.category.id;
+            const title = element.title;
+            const category = element.category;
+            const products = await strapi.services.product.find({
+              "category.id": id,
+            });
+            if (products.length) {
+              for (const product of products) {
+                delete product?.created_by;
+                delete product?.updated_by;
+                delete product?.published_at;
+                delete product?.created_at;
+                delete product?.updated_at;
+                delete product?.category;
+              }
+            }
+            return { title, category, products };
+          })
+        );
+        section.items = newItems;
+      }
+    }
+
+    return content;
+  },
+};
