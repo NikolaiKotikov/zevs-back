4c920cce54443ea08fbb5d26515d1d1a622bd820
nik <nikolai.kotikov@gmail.com>
2021-09-27 19:47:26 +0300

    feat: :bug: add url to server config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://zeus.kotins.website/api",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '16d2c90b40a1dc691510030f760cf1fe'),
+      secret: env("ADMIN_JWT_SECRET", "16d2c90b40a1dc691510030f760cf1fe"),
     },
   },
 });
