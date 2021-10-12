9a45d5941e88f9b01099497467895dc8dc1e751a
nik <nikolai.kotikov@gmail.com>
2021-10-12 06:54:31 +0300

    feat: :rocket: configure server.js with .env variable

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://zeus.kotins.website/api",
+  url: env('PUBLIC_URL', ''),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "16d2c90b40a1dc691510030f760cf1fe"),
