950813374037542eaae2df61f54736c4e36a8d24
nkotikov <nkotikov@moysklad.ru>
2023-08-13 17:50:59 +0300

    add telegram notifications on feedback model create
    also fix node version

--- api/feedback/models/feedback.js
+++ api/feedback/models/feedback.js
@@ -1,8 +1,34 @@
 'use strict';
 
+const { Telegraf } = require('telegraf');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const BOT_TOKEN = process.env.TG_BOT_TOKEN
+const CHAT_ID = process.env.TG_CHAT_ID
+
+const bot = new Telegraf(BOT_TOKEN)
+
+const VALUE_NOT_PROVIDED = 'не заполнено'
+const getValueOrDefault = (value) => value ?? VALUE_NOT_PROVIDED
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      const message = `
+<u>*** Новый Заказ №${result.id} ***</u>
+<b>Имя</b>: ${getValueOrDefault(result.name)}
+<b>Телефон</b>: <code>${getValueOrDefault(result.phone)}</code>
+<b>Email</b>: <code>${getValueOrDefault(result.email)}</code>
+<b>Комментарий</b>: ${getValueOrDefault(result.message)}
+      `
+      try {
+        await bot.telegram.sendMessage(CHAT_ID, message, {parse_mode: 'HTML'})
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
